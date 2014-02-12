9069f2bfd8fcb792af8d7688fc4a47e36ee4df9d
manzell <manzell.blakeley@gmail.com>
2014-02-12 15:14:31 -0500

    Update maps.google.polygon.containsLatLng.js
    Added functionality for Circles; moved to the MVC object rather than polygons. Maybe it would be better to implement the same function across two prototypes but whatever.

--- maps.google.polygon.containsLatLng.js
+++ maps.google.polygon.containsLatLng.js
@@ -5,7 +5,7 @@ if (!google.maps.Polygon.prototype.getBounds) {
     var bounds = new google.maps.LatLngBounds();
     var paths = this.getPaths();
     var path;
-    
+
     for (var p = 0; p < paths.getLength(); p++) {
       path = paths.getAt(p);
       for (var i = 0; i < path.getLength(); i++) {
@@ -17,52 +17,52 @@ if (!google.maps.Polygon.prototype.getBounds) {
   }
 }
 
-// Polygon containsLatLng - method to determine if a latLng is within a polygon
-google.maps.Polygon.prototype.containsLatLng = function(latLng) {
-  // Exclude points outside of bounds as there is no way they are in the poly
- 
-  var lat, lng;
+google.maps.MVCObject.prototype.containsPoint = function(latLng) {
+	var lat, lng;
 
-  //arguments are a pair of lat, lng variables
-  if(arguments.length == 2) {
-    if(typeof arguments[0]=="number" && typeof arguments[1]=="number") {
-      lat = arguments[0];
-      lng = arguments[1];
-    }
-  } else if (arguments.length == 1) {
-    var bounds = this.getBounds();
+	if(arguments.length == 2 && typeof arguments[0] == 'number' && typeof arguments[1] == 'number') {
+		lat = arguments[0];
+		lng = arguments[1];
+	} else if(arguments.length == 1) {
+		var bounds = this.getBounds();
 
-    if(bounds != null && !bounds.contains(latLng)) {
-      return false;
-    }
-    lat = latLng.lat();
-    lng = latLng.lng();
-  } else {
-    console.log("Wrong number of inputs in google.maps.Polygon.prototype.contains.LatLng");
-  }
+		if(bounds != null && !bounds.contains(latLng)) return false;
 
-  // Raycast point in polygon method
-  var inPoly = false;
+		lat = latLng.lat();
+		lng = latLng.lng();
+	} else {
+		console.log("Error - wrong number of arguments");
+	}
 
-  var numPaths = this.getPaths().getLength();
-  for(var p = 0; p < numPaths; p++) {
-    var path = this.getPaths().getAt(p);
-    var numPoints = path.getLength();
-    var j = numPoints-1;
+	var inPoly = false;
 
-    for(var i=0; i < numPoints; i++) { 
-      var vertex1 = path.getAt(i);
-      var vertex2 = path.getAt(j);
+	if(this instanceof google.maps.Polygon) {
+		// Raycast point in polygon method
+		var numPaths = this.getPaths().getLength();
+		for(var p = 0; p < numPaths; p++) {
+			var path = this.getPaths().getAt(p);
+			var numPoints = path.getLength();
+			var j = numPoints-1;
 
-      if (vertex1.lng() < lng && vertex2.lng() >= lng || vertex2.lng() < lng && vertex1.lng() >= lng) {
-        if (vertex1.lat() + (lng - vertex1.lng()) / (vertex2.lng() - vertex1.lng()) * (vertex2.lat() - vertex1.lat()) < lat) {
-          inPoly = !inPoly;
-        }
-      }
+			for(var i=0; i < numPoints; i++) {
+				var vertex1 = path.getAt(i);
+				var vertex2 = path.getAt(j);
 
-      j = i;
-    }
-  }
+				if (vertex1.lng() < lng && vertex2.lng() >= lng || vertex2.lng() < lng && vertex1.lng() >= lng) {
+					if (vertex1.lat() + (lng - vertex1.lng()) / (vertex2.lng() - vertex1.lng()) * (vertex2.lat() - vertex1.lat()) < lat) {
+					  inPoly = !inPoly;
+					}
+				}
 
-  return inPoly;
+				j = i;
+			}
+		}
+	}
+	else if(this instanceof google.maps.Circle) {
+		src = this.getCenter();
+		dest = new google.maps.LatLng(lat, lng);
+		inPoly = google.maps.geometry.spherical.computeDistanceBetween(src, dest) <= this.radius;
+	}
+
+	return inPoly;
 }
